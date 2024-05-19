71abf8ee3bf3ce022bad84dac2887856dae0f121
undeadhecate <86398409+undeadhecate@users.noreply.github.com>
2024-05-19 17:56:16 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from flask import Flask, render_template, request,redirect,url_for
 import pymongo
-
+from bson import ObjectId
+from dateutil import relativedelta
+from datetime import datetime,timedelta
+import uuid
 app = Flask(__name__)
 
 myclient = pymongo.MongoClient("mongodb://localhost:27017/")
@@ -184,10 +187,44 @@ def manageuser():
         mycol = mydb["Users"]
         for x in mycol.find():
             users.append(x)
+        print(users)
 
 
 
     return render_template('ManageUsers.html',RestrictionsAvailable=RestrictionsAvailable,roles=roles,users=users)
+
+@app.route('/EditUser', methods=["POST"])
+def edituser():
+    global mydb
+
+    mycol = mydb["Users"]
+    userdetails = {}
+    restrictions = []
+    counter = 0
+    for i in request.form.items() :
+        if counter==0:
+            pass
+        elif counter == 1 :
+            userdetails['name'] = i[1]
+        elif counter == 2 :
+            userdetails['role'] = i[1]
+        elif counter == 3 :
+            userdetails['contact'] = i[1]
+        else :
+            restrictions.append(i[0])
+        counter += 1
+    userdetails['restrictions'] = restrictions
+
+    myquery = {"_id": ObjectId(str(request.form["OldUserName"]))}
+    newvalues = {"$set": userdetails}
+    print(userdetails)
+    print(mycol.update_one(myquery, newvalues))
+    return redirect(url_for('manageuser'))
+
+
+
+
+
 
 @app.route('/CreateManning',methods=["GET","POST"])
 def createmanning():
@@ -204,6 +241,22 @@ def createmanning():
         return redirect(url_for('ManageManning'))
 
     return render_template('CreateManning.html')
+
+@app.route('/CreateShiftTemplate',methods=["GET","POST"])
+def createshifttemplate():
+    global mydb
+
+    if request.method=="POST":
+
+
+        mycol = mydb["Manning"]
+
+        mydict = {"name": request.form['Name'],"StartDate": request.form['StartDate'],"StartTime": request.form['StartTime'],"EndDate": request.form['EndDate'],"EndTime": request.form['EndTime'],"Data":[]}
+
+        x = mycol.insert_one(mydict)
+        return redirect(url_for('ManageManning'))
+
+    return render_template('CreateShiftTemplate.html')
 
 @app.route('/ManageDutyPost', methods=["GET","POST"])
 def ManageDutyPost():
@@ -256,15 +309,66 @@ def ManageManning():
     mycol = mydb["Manning"]
     manning=[]
     for x in mycol.find():
-        manning.append(x['name'])
+        manning.append(x)
+    print(manning)
 
 
     return render_template('ManageManning.html',manningdata=manning)
 
-@app.route('/ViewManning')
-def ViewManning():
-    return render_template('ViewManning.html')
+@app.route('/ViewManning/<id>',methods=["POST","GET"])
+def ViewManning(id):
+    mycol = mydb["Manning"]
+
+
+    if request.method=="POST":
+        x = mycol.find_one({"_id":ObjectId(str(id))})
+        templist= {"DP": request.form['DP'],"StartDate": request.form['StartDate'],"StartTime": request.form['StartTime'],"EndDate": request.form['EndDate'],"EndTime": request.form['EndTime'],'uid':str(uuid.uuid4())}
+        x['Data'].append(templist)
+        myquery = {"_id":ObjectId(str(id))}
+        newvalues = {"$set" : x}
+
+        mycol.update_one(myquery, newvalues)
+        return redirect(url_for("ViewManning",id=id))
+
+
+    data=mycol.find_one({"_id": ObjectId(str(id))})
+
+
+    # Parse the dates from strings into datetime objects
+    date1 = datetime.strptime(data['StartDate']+" "+data["StartTime"], "%d%m%y %H%M")
+    date2 = datetime.strptime(data['EndDate']+" "+data["EndTime"], "%d%m%y %H%M")
+    # Calculate the difference between the two dates
+    difference = relativedelta.relativedelta(date2, date1)
+
+    hours=difference.days*24
+    hours+=difference.hours+1
+
+    timestamps={}
+    for i in range(hours):
+        timestamps[((date1+timedelta(hours=i)).strftime("%d%m%y %H%M") )]={}
+
+    mycol = mydb["DutyPosts"]
+    dutyposts=mycol.find()
+    dutypostsnames=[]
+    for i in dutyposts:
+        dutypostsnames.append(i['name'])
+    for i in data['Data']:
+        selector=((datetime.strptime(i['StartDate']+" "+i["StartTime"], "%d%m%y %H%M")).strftime("%d%m%y %H%M") )
+        olddata=timestamps[selector]
+        date1 = datetime.strptime(i['StartDate'] + " " + i["StartTime"], "%d%m%y %H%M")
+        date2 = datetime.strptime(i['EndDate'] + " " + i["EndTime"], "%d%m%y %H%M")
+        # Calculate the difference between the two dates
+        difference = relativedelta.relativedelta(date2, date1)
+        hours = difference.days * 24
+        hours += difference.hours
+
+        olddata[i['DP']]=[hours,i['uid'],i['StartDate'],i["StartTime"],i['EndDate'],i["EndTime"],]
+        print(olddata)
+        timestamps[selector]=olddata
+
+    print(timestamps)
+    return render_template('ViewManning.html',data=data,dutypostsnames=dutypostsnames,timestamps=timestamps)
 
 if __name__ == '__main__':
 
-    app.run(debug=True, host='192.168.1.64', port=5000)
+    app.run(debug=True, host='127.0.0.1', port=5000)
